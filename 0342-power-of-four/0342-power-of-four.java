cf4ac8abf7eb00db59f4fc4469e187ab9d2f6d79
AidarSource <34319725+AidarSource@users.noreply.github.com>
2022-12-31 17:49:36 +0600

    Time: 1 ms (100.00%), Space: 39.5 MB (84.63%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n < 1) return false;
-
-        while(n % 4 == 0) {
-            n /= 4;
-        }
-
-        return n == 1;
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 }
