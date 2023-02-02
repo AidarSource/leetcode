b5dbecde08b4a332992666a455fd1eadad255f3e
AidarSource <34319725+AidarSource@users.noreply.github.com>
2023-02-03 01:49:00 +0600

    Time: 4 ms (17.97%), Space: 42.8 MB (33.65%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    Stack<Character> stack = new Stack<>();
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        backtrack(0, 0, n);
+
+        return res;
+    }
+
+    private void backtrack(int openN, int closedN, int n) {
+        if(openN == closedN && closedN == n) {
+            Iterator val = stack.iterator();
+            String temp = "";
+            while(val.hasNext()) {
+                temp = temp + val.next();
+            }
+            res.add(temp);
+        }
+        if(openN < n) {
+            stack.push('(');
+            backtrack(openN + 1, closedN, n);
+            stack.pop();
+        }
+        if(closedN < openN) {
+            stack.push(')');
+            backtrack(openN, closedN + 1, n);
+            stack.pop();
+        }
+    }
+}
