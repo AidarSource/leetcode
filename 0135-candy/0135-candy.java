5762bb12d2974c5f6998ff4ed0bbbede40d814c7
AidarSource <34319725+AidarSource@users.noreply.github.com>
2023-09-14 00:52:54 +0600

    Time: 2 ms (99.93%), Space: 44.3 MB (20.36%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int totalCandies = 0;
+
+        int[] distribution = new int[ratings.length];
+
+        Arrays.fill( distribution, 1 );
+
+        for( int i = 0; i < ratings.length - 1; i++ ) {
+            if( ratings[i + 1] > ratings[i] ) {
+                distribution[i + 1] = distribution[i] + 1;
+            }
+        }
+
+        for( int i = ratings.length - 1; i > 0; i-- ) {
+            if( ratings[i - 1] > ratings[i] ) {
+                if( distribution[i - 1] <= ( distribution[i] ) ) {
+                    distribution[i - 1] = distribution[i] + 1;
+                }
+            }
+        }
+
+        for( int i = 0; i < distribution.length; i++ ) {
+            totalCandies += distribution[i];
+        }
+        
+        return totalCandies;
+    }
+}
