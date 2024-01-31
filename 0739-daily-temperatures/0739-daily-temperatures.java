4e3399b58b99c88d1e77aca7815de4e56dc7fa25
AidarSource <34319725+AidarSource@users.noreply.github.com>
2024-01-31 22:51:27 +0600

    Time: 61 ms (69.51%), Space: 63.4 MB (5.12%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() &&
+             temperatures[i] > temperatures[stack.peek()]) {
+                 int prevDay = stack.pop();
+                 res[prevDay] = i - prevDay;
+             }
+             stack.add(i);
+        }
+
+        return res;
+    }
+}
