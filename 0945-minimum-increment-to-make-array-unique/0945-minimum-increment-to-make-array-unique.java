6741e49a69c6b78105001a53c3b824551cac85dc
AidarSource <34319725+AidarSource@users.noreply.github.com>
2024-06-14 12:09:01 +0500

    Time: 36 ms (79.67%), Space: 60 MB (51.48%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        // Sort the array first
+        Arrays.sort(nums);
+
+        int numTracker = 0;
+        int minIncrement = 0;
+
+        for (int num : nums) {
+            numTracker = Math.max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1;
+        }
+
+        return minIncrement;
+    }
+}
