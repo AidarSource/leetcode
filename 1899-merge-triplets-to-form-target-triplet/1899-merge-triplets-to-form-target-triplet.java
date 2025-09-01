9aa1a7626f8de8ce9cb2226f55824a18038fe4ee
AidarSource <34319725+AidarSource@users.noreply.github.com>
2025-09-01 15:47:25 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        Set<Integer> good = new HashSet<>();
+
+        for(int[] t : triplets) {
+            if(t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+
+            for(int i = 0; i < t.length; i++) {
+                if(t[i] == target[i]) {
+                    good.add(t[i]);
+                }
+            }
+        }
+
+        return len(good) == 3;
+    }
+}
