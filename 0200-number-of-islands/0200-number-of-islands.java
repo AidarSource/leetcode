c9509055ba536d6939b51df0788b3a321ae7c6b9
AidarSource <34319725+AidarSource@users.noreply.github.com>
2026-01-17 23:27:36 +0500

    Time: 5 ms (33.12%), Space: 51.3 MB (92.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,38 @@
-public class Solution {
-
-    private int n;
-    private int m;
-
+class Solution {
+    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int numIslands(char[][] grid) {
-        int count = 0;
-        n = grid.length;
-        if (n == 0) return 0;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
+        int rows = grid.length, cols = grid[0].length;
+        int islands = 0;
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == '1') {
+                    bfs(grid, r, c);
+                    islands++;
                 }
-        }    
-        return count;
+            }
+        }
+
+        return islands;
     }
 
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+    private void bfs(char[][] grid, int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        grid[r][c] = '0';
+        q.add(new int[]{r, c});
+
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            int row = node[0], col = node[1];
+
+            for(int[] dir : directions) {
+                int nr = row + dir[0], nc = col + dir[1];
+                if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length &&
+                grid[nr][nc] == '1') {
+                    q.add(new int[]{nr, nc});
+                    grid[nr][nc] = '0';
+                }
+            }
+        }
     }
 }
